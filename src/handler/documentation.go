f51b3b85e9ded3861f604da81970af635b9eb6a1
Leonardo-Antonio <leo2001.nl08@gmail.com>
2021-09-14 23:10:11 -0500

    redirect documentation

--- src/handler/documentation.go
+++ src/handler/documentation.go
@@ -3,10 +3,7 @@ package handler
 import (
 	"net/http"
 
-	"github.com/Leonardo-Antonio/api.lyabook/src/entity"
-	"github.com/Leonardo-Antonio/api.lyabook/src/utils/response"
 	"github.com/labstack/echo/v4"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type documentation struct{}
@@ -16,9 +13,5 @@ func NewDocumentation() *documentation {
 }
 
 func (d *documentation) Index(ctx echo.Context) error {
-	u := entity.User{
-		Id:   primitive.NewObjectID(),
-		Name: "Ñepd",
-	}
-	return response.New(ctx, http.StatusOK, "todo good", false, u)
+	return ctx.Redirect(http.StatusPermanentRedirect, "https://documenter.getpostman.com/view/16524197/U16onNMr")
 }
