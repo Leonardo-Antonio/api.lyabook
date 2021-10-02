0d4b9f87509e14ab02387c4b1675f0b3f44f74f7
Leonardo-Antonio <leo2001.nl08@gmail.com>
2021-10-02 07:14:28 -0500

    doc

--- src/handler/documentation.go
+++ src/handler/documentation.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Leonardo-Antonio/api.lyabook/src/utils/response"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,6 +13,10 @@ func NewDocumentation() *documentation {
 	return &documentation{}
 }
 
-func (d *documentation) Index(ctx echo.Context) error {
+func (d *documentation) Doc(ctx echo.Context) error {
 	return ctx.Redirect(http.StatusPermanentRedirect, "https://documenter.getpostman.com/view/16524197/U16onNMr")
+}
+
+func (d *documentation) Index(ctx echo.Context) error {
+	return response.New(ctx, http.StatusOK, "bienvenido a la api de lyabook develop", false, nil)
 }
--- src/router/documentation.go
+++ src/router/documentation.go
@@ -2,12 +2,10 @@ package router
 
 import (
 	"github.com/Leonardo-Antonio/api.lyabook/src/handler"
-	"github.com/Leonardo-Antonio/api.lyabook/src/utils/env"
 	"github.com/labstack/echo/v4"
 )
 
 func Documentation(e *echo.Echo) {
 	doc := handler.NewDocumentation()
-	group := e.Group(env.Data.BaseUrl)
-	group.GET("", doc.Index)
+	e.GET("/", doc.Index)
 }
